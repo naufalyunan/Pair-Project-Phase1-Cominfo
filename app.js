336242755046cf7dc5832ede50ea95e9ad6f1f3f
naufalyunan <naufalyunan45@gmail.com>
2020-02-28 03:10:54 +0700

    edit app.js port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3004;
+const port = process.env.PORT || 4000;
 const router = require('./routers/index');
 const session = require('express-session');
 
