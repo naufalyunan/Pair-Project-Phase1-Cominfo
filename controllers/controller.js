e896a13a5c9a67de34bf508cbe1348d73bc3c3af
naufalyunan <naufalyunan45@gmail.com>
2020-02-27 15:42:44 +0700

    commit to merge login

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,8 +1,37 @@
-const { Trader } = require('./../models')
+const { Trader, Commodity, CommodityTrader } = require('./../models')
 class Controller{
     static getHome(req,res){
         // res.send('WELCOME TO HOME')
-        res.render('home')
+        Commodity.findAll()
+            .then(commodities => {
+                CommodityTrader.aggregate('location','DISTINCT',{ plain: false })
+                    .then(result => {
+                        CommodityTrader.findAll({
+                            attributes: [
+                                [ sequelize.fn('date_trunc', 'hour', sequelize.col('date')), 'hour'],
+                            ],
+                            group: 'hour',
+                            include: [Trader, Commodity]
+                        })
+                            .then(commodityTraders => {
+                                console.log(commodityTraders);
+                                
+                                res.render('home', { commodities, result, commodityTraders })
+
+                            })
+                            .catch(err => {
+                                console.log(err)
+                                res.send(err)
+                            })
+                        
+                    })
+                    .catch(err => {
+                        res.send(err)
+                    })
+            })
+            .catch(err => {
+                res.send(err)
+            })
     }
     static getLogin(req,res){
         res.render('loginPage')
