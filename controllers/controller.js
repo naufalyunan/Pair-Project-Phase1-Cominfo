b6f496e796da152bfeff0e38ef1e88aeb7eaf941
naufalyunan <naufalyunan45@gmail.com>
2020-02-27 21:34:15 +0700

    add login error, search by name and market

--- controllers/controller.js
+++ controllers/controller.js
@@ -44,9 +44,13 @@ class Controller{
                                     let priceArr = []
                                     for(let i = 0; i < filtCommTraders.length; i++){
                                         let name = filtCommTraders[i].dataValues.Commodity.dataValues.name
+                                        let image = filtCommTraders[i].dataValues.Commodity.dataValues.image
+                                        let id = filtCommTraders[i].dataValues.Commodity.dataValues.id
                                         let price = filtCommTraders[i].dataValues.price
                                         if(unique[j] === name){
+                                            newObj.id = id;
                                             newObj.name = name;
+                                            newObj.image = image;
                                             priceArr.push(price)
                                         } 
                                             
@@ -61,7 +65,92 @@ class Controller{
                                 }
                                     
                                 console.log(newArr);
-                                res.render('home', { commodities, result, commodityTraders: newArr })
+                                console.log(req.session);
+
+                                res.render('home', { commodities, result, commodityTraders: newArr, req })
+
+                            })
+                            .catch(err => {
+                                console.log(err)
+                                res.send(err)
+                            })
+                        
+                    })
+                    .catch(err => {
+                        res.send(err)
+                    })
+            })
+            .catch(err => {
+                res.send(err)
+            })
+    }
+
+    static getHomeVar(req,res){
+        let bulan = Number(req.params.bulan)
+        let tahun = Number(req.params.tahun)
+        Commodity.findAll()
+            .then(commodities => {
+                CommodityTrader.aggregate('location','DISTINCT',{ plain: false })
+                    .then(result => {
+                        CommodityTrader.findAll({
+                            include: [Trader, Commodity]
+                        })
+                            .then(commodityTraders => {
+                                commodityTraders.forEach(el => {
+                                    let date = el.dataValues.date
+                                    let month = date.getMonth()
+                                    let day = date.getDate()
+                                    let year = date.getFullYear()
+                                    el.dataValues.month = month + 1
+                                    el.dataValues.day = day
+                                    el.dataValues.year = year
+                                })
+                                
+                                // untuk bulan Jan 2019
+                                let filtCommTraders = [];
+                                commodityTraders.forEach(el => {
+                                    if(el.dataValues.month === bulan && el.dataValues.year === tahun) {
+                                        filtCommTraders.push(el)
+                                    }
+                                })
+                                let newArr = []
+                                let numberOfComm = {
+                                    name : []
+                                }
+                                filtCommTraders.forEach(el => {
+                                    numberOfComm.name.push(el.dataValues.Commodity.dataValues.name)
+                                })
+                                function onlyUnique(value, index, self) { 
+                                    return self.indexOf(value) === index;
+                                }
+                                let unique = numberOfComm.name.filter( onlyUnique );
+                                for (let j = 0; j < unique.length; j++) {
+                                    let newObj = {}
+                                    let priceArr = []
+                                    for(let i = 0; i < filtCommTraders.length; i++){
+                                        let name = filtCommTraders[i].dataValues.Commodity.dataValues.name
+                                        let image = filtCommTraders[i].dataValues.Commodity.dataValues.image
+                                        let id = filtCommTraders[i].dataValues.Commodity.dataValues.id
+                                        let price = filtCommTraders[i].dataValues.price
+                                        if(unique[j] === name){
+                                            newObj.id = id;
+                                            newObj.name = name;
+                                            newObj.image = image;
+                                            priceArr.push(price)
+                                        } 
+                                            
+                                        if(i === filtCommTraders.length - 1) {
+                                            newObj.price = priceArr;
+                                        }    
+                                        console.log(priceArr);
+                                        
+                                    }
+                                    newObj.avg = CommodityTrader.getAverage(newObj.price)
+                                    newArr.push(newObj)
+                                }
+                                    
+                                console.log(newArr);
+                                res.render('home', { commodities, result, commodityTraders: newArr, req })
 
                             })
                             .catch(err => {
@@ -79,7 +168,7 @@ class Controller{
             })
     }
     static getLogin(req,res){
-        res.render('login-form')
+        res.render('login-form', { errors: null })
     }
     static postLogin(req,res){
         let { username,password } = req.body
@@ -93,6 +182,7 @@ class Controller{
                 let trader = result[0].dataValues
                 if(trader.password === password){
                     req.session.isLogin = true
+                    req.session.username = username
                 } else {
                     throw new Error(`Password is incorrect!`)
                 }
@@ -101,7 +191,7 @@ class Controller{
             })
             .catch(err => {
                 console.log(err)
-                res.send(err)
+                res.render('login-form', { errors:err })
             })
     }
     static logout(req,res){
@@ -109,6 +199,7 @@ class Controller{
             if (err) {
                 res.send(err)
             } else {
+                console.log(`logout`);
                 res.redirect('/')
             }
         });
--- routers/conjunction.js
+++ routers/conjunction.js
@@ -2,10 +2,17 @@ const router = require('express').Router();
 const ControllerConj = require('./../controllers/controllerConj')
 
 router.get('/', ControllerConj.getConj)
-router.get('/add', ControllerConj.getAdd)
+router.get('/add', (req,res, next) => {
+    if (req.session.isLogin) {
+        next()
+    } else {
+        throw new Error('You need to login first')
+    }
+} , ControllerConj.getAdd)
 router.post('/add', ControllerConj.postAdd)
 router.get('/edit/:id', ControllerConj.getEdit)
 router.post('/edit/:id', ControllerConj.postEdit)
 router.get('/delete/:id', ControllerConj.delete)
+router.get('/:market', ControllerConj.getByMarket)
 
 module.exports = router
